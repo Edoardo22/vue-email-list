b630abf5208d21a153eb4754b9981e292a254e57
Edoardo22 <vanaliedoardo@gmail.com>
2022-06-17 15:58:19 +0200

    es finito

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ new Vue(
         methods:
         {
             generateEmailRandom() {
+                this.emailList = [];
                 for (let i = 0; i < 10; i++) {
                     axios
                         .get("https://flynn.boolean.careers/exercises/api/random/mail")
